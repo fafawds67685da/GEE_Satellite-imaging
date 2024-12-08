2f77ddb094b87dcf5f7282f78e8e5a24658e1dbb
Dev goyal <158808178+fafawds67685da@users.noreply.github.com>
2024-12-08 14:51:17 +0530

    Create Final 03.js

---
+++ Final Work/Final 03.js
@@ -0,0 +1,66 @@
+var sentinel2 = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED");
+
+var image = sentinel2.filterBounds(ROI)
+    .filterDate('2018-01-01', '2018-12-1')
+    .filterMetadata('CLOUDY_PIXEL_PERCENTAGE', 'less_than', 1)
+    .median()
+    .clip(ROI);
+Map.addLayer(image,imageVisParam,"Real image")//     4-3-2
+Map.addLayer(image,imageVisParam4, "Every layer")// 11-8-2
+Map.addLayer(image,imageVisParam3, "URBAN")//       11-4-8
+
+
+
+var bands = ['B2', 'B3', 'B4', 'B8', 'B11'];
+
+
+var sample = Water.merge(Vegetation).merge(Baren).merge(Forest).merge(Urban).merge(Dry_soil)
+
+
+var training = image.select(bands).sampleRegions({
+  collection: sample,
+  properties: ['Class'], // Class property for ground truth
+  scale: 10
+});
+
+//print(sample);
+
+var dataset = training.randomColumn(); // Add a random column for splitting
+
+var trainset = dataset.filter(ee.Filter.lessThan('random', 0.80));
+var testset = dataset.filter(ee.Filter.greaterThanOrEquals('random', 0.80));
+
+// Train a Random Forest classifier
+var model_train = ee.Classifier.smileRandomForest(20).train({
+  features: trainset, // Training set
+  classProperty: 'Class', // Ground truth property
+  inputProperties: bands // Features used for training
+});
+
+// Classify the entire image using the trained model
+var LULC_train = image.select(bands).classify(model_train);
+
+// Add the classified map to the viewer
+Map.addLayer(LULC_train, {min: 0, max: 5, palette: ['blue', 'yellow', 'grey', 'green','red','orange']}, 'LULC_train Map');
+
+
+
+/*var LULC_test = image.select(bands).classify(model_train);
+
+Map.addLayer(LULC_test, {min: 0, max: 4, palette: ['blue', 'yellow', 'grey', 'green','red']}, 'LULC_test Map');*/
+
+
+// Classify the testset using the trained model
+var testClassification = testset.classify(model_train);
+
+// Generate a confusion matrix
+var confusionMatrix = testClassification.errorMatrix('Class', 'classification');
+print('Confusion Matrix:', confusionMatrix);
+
+// Overall accuracy
+var accuracy = confusionMatrix.accuracy();
+print('Overall Accuracy:', accuracy);
+
+// Kappa coefficient
+var kappa = confusionMatrix.kappa();
+print('Kappa Coefficient:', kappa);
